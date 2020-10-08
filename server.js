c3f1e7939e9d943c1eb1fd28a6bdde8ab74c4c60
Danial-Alizdh <58402864+Danial-Alizdh@users.noreply.github.com>
2020-10-08 19:39:46 +0330

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
+const multer = require('multer');
+const crypto = require('crypto');
 const app = express();
 
 const log = console.log;
@@ -60,14 +62,47 @@ app.get('/sendemail/:to/:subject/:text/:fileAddress?/:fileType?',
 		sendEmail(res, req.params.to, req.params.subject, req.params.text);
 });
 
-app.get('/upload/:image', (req, res) => {
+/*app.get('/upload/:image', (req, res) => {
 	return res.json(req.params);
-});
+});*/
 
 app.post('/', function (req, res) {
   //var file = req.pipe(fs.createWriteStream('./uploadFile'));
   //req.on('end', next);
   return res.json({message : req.json('image')});
+});
+
+storage = multer.diskStorage({
+    destination: './uploads/',
+    filename: function(req, file, cb) {
+      return crypto.pseudoRandomBytes(16, function(err, raw) {
+        if (err) {
+          return cb(err);
+        }
+        return cb(null, "" + (raw.toString('hex')) + (path.extname(file.originalname)));
+      });
+    }
+  });
+
+
+// Post files
+app.post("/upload",
+  multer({
+    storage: storage
+  }).single('upload'), function(req, res) {
+    console.log(req.file);
+    console.log(req.body);
+    res.redirect("/uploads/" + req.file.filename);
+    console.log(req.file.filename);
+    return res.status(200).end();
+  });
+
+app.get('/uploads/:upload', function (req, res){
+  file = req.params.upload;
+  console.log(req.params.upload);
+  var img = fs.readFileSync(__dirname + "/uploads/" + file);
+  res.writeHead(200, {'Content-Type': 'image/png' });
+  res.end(img, 'binary');
 });
 
 app.listen(PORT, () => log('Server is starting on PORT,', 8080));
