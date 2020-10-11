d9458a3b30effdc2c6c29805393bc42e285cd795
Danial-Alizdh <58402864+Danial-Alizdh@users.noreply.github.com>
2020-10-11 23:34:04 +0330

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/sendemail/:subject/:text/:fileAddress?/:fileType?',
 
 function base64_decode(base64str, file) {
     // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-    var bitmap = new Buffer.from(base64str, 'base64');
+    var bitmap = new Buffer(base64str, 'base64');
     // write buffer to file
     var image = fs.writeFileSync(file, bitmap);
     console.log('******** File created from base64 encoded string ********');
