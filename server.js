d7ba029be2348a151827ae6d1344a856c108bd13
Danial-Alizdh <58402864+Danial-Alizdh@users.noreply.github.com>
2020-10-12 15:59:52 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(fileUpload({
 	  },
 }));
 
-app.use(express.json());
-app.use(express.limit("5mb"));
+app.use(express.json({limit: '15mb'}));
+// app.use(express.limit('6M'));
 
 const log = console.log;
 const PORT = process.env.PORT || 8080;
