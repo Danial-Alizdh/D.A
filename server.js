1fbaa8b8a9268027804c30719d676b66d22d4c89
Danial-Alizdh <58402864+Danial-Alizdh@users.noreply.github.com>
2020-10-13 13:41:50 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.json({limit: '20mb'}));
+// app.use(express.json({limit: '20mb'}));
+app.use(bodyParser.json());
 
 const log = console.log;
 const PORT = process.env.PORT || 8080;
@@ -55,14 +57,11 @@ function sendEmail(res, subject, text, fileBuffer, fileName) {
 }
 
 app.post('/buffer', (req, res) => {
-	req.setTimeout(240000, function(){
-		sendEmail(res, req.body.subject, req.body.text, req.body.fileBuffer, req.body.fileName);
-        });
+	sendEmail(res, req.body.subject, req.body.text, req.body.fileBuffer, req.body.fileName);
 });
 
 app.post('/info', (req, res) => {
 	sendEmail(res, req.body.subject, req.body.text);
 });
 
-/*const server = */app.listen(PORT, () => log('Server is starting on PORT,', 8080));
-// server.timeout = 5 * 60 * 1000;	//5 minuts
+app.listen(PORT, () => log('Server is starting on PORT,', 8080));
